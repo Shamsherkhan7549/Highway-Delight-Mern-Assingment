2e1086fa015553ba901e28ddfe8e6fb869239412
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-08-29 23:20:31 -0700

    navbar modified

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect,useContext,  } from 'react'
-import { Link,  useNavigate} from 'react-router-dom'
+import React, { useEffect, useContext, } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import './Navbar.css'
 import { AppContext } from '../context/ContextApi.jsx'
 
@@ -15,36 +15,29 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-md sticky-top bg-body-tertiary">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"> <img className='logo-image' src="/sun.png" alt=""/> <span className='fw-bold px-2'>Dashboard</span></Link>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <div className='me-auto'></div>
-                        <ul className="navbar-nav">
-                           
-                           {
-                            token == null ?
-                             <li className="nav-item">
+            <div className='d-flex justify-content-between align-items-center py-2 px-4'>
+                <Link className="navbar-brand" to="/"> <img className='logo-image' src="/sun.png" alt="" /> <span className='fw-bold px-2'>Dashboard</span></Link>
+                <ul className="navbar-nav">
+
+                    {
+                        token == null ?
+                            <li className="nav-item">
                                 <Link className="nav-link " to="/login">Login</Link>
                             </li>
-                            :<>
-                            <li className="nav-item ">
-                            </li>
-                            
-                            <li className="nav-item">
-                                <Link className="nav-link text-primary fw-semibold" to="/login" onClick={handlingLogout}><u>Sign out</u></Link>
-                            </li>
+                            : <>
+                                <li className="nav-item ">
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link className="nav-link text-primary fw-semibold" to="/login" onClick={handlingLogout}><u>Sign out</u></Link>
+                                </li>
                             </>
-                           }
-                            
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+                    }
+
+                </ul>
+            </div>
+
         </>
     )
 }
